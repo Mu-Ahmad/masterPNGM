f9c18c108d6a1d1e365a95de3e71ccb88b21fc02
Muhammad Ahmad <ahmadsaifofficial@gmail.com>
2023-05-02 23:50:54 +0500

    Fixes

--- api/views.py
+++ api/views.py
@@ -16,27 +16,32 @@ def other(request):
 
 @api_view((['Get']))
 def generate(request):
-	prime_range = math.floor(10**3/3)
-
-	url = 'http://web:8000/api/generate'
-	params = {'from': 1, 'to': prime_range}
-	requests.get(url, params=params)
-
-	url = 'http://web-1:8000/api/generate'
-	params = {'from': prime_range+1, 'to': prime_range*2}
-	requests.get(url, params=params)
-
-	url = 'http://web-2:8000/api/generate'
-	params = {'from': prime_range*2+1, 'to': prime_range*3+1}
-	requests.get(url, params=params)
 
 	thread = threading.Thread(target=periodic, args=(60, log_performance))
 	thread.daemon = True
 	thread.start()
 
-	thread1 = threading.Thread(target=periodic, args=(120, fetch_primes()))
+	thread1 = threading.Thread(target=periodic, args=(120, fetch_primes))
 	thread1.daemon = True
 	thread1.start()
+
+
+	prime_range = math.floor(10**12/3)
+
+	url = 'http://web:8000/api/generate'
+	params = {'from': 1, 'to': prime_range}
+	requests.get(url, params=params)
+	print('printed', 1, prime_range)
+
+	url = 'http://web-1:8000/api/generate'
+	params = {'from': prime_range+1, 'to': prime_range*2}
+	requests.get(url, params=params)
+	print('printed', prime_range+1, prime_range*2)
+
+	url = 'http://web-2:8000/api/generate'
+	params = {'from': prime_range*2+1, 'to': prime_range*3+1}
+	requests.get(url, params=params)
+	print('printed', prime_range*3+1)
 
 	return Response({'response' : 'Master Service Invoked.....'})
 
